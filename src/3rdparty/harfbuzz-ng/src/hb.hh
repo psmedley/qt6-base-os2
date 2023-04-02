8f2720379521b09a94561d898205538ab1d605df
Paul Smedley <paul@smedley.id.au>
2023-04-02 18:21:44 +0930

    Don't use __visibility__(hidden) on OS/2
    Signed-off-by: Paul Smedley <paul@smedley.id.au>

--- src/3rdparty/harfbuzz-ng/src/hb.hh
+++ src/3rdparty/harfbuzz-ng/src/hb.hh
@@ -273,7 +273,7 @@ extern "C" void  hb_free_impl(void *ptr);
 #endif
 
 #ifndef HB_INTERNAL
-# if !defined(HB_NO_VISIBILITY) && !defined(__MINGW32__) && !defined(__CYGWIN__) && !defined(_MSC_VER) && !defined(__SUNPRO_CC)
+# if !defined(HB_NO_VISIBILITY) && !defined(__MINGW32__) && !defined(__CYGWIN__) && !defined(_MSC_VER) && !defined(__SUNPRO_CC) && !defined(__OS2__)
 #  define HB_INTERNAL __attribute__((__visibility__("hidden")))
 # elif defined(__MINGW32__)
    /* We use -export-symbols on mingw32, since it does not support visibility attributes. */
